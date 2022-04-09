6e6ded53597cae72ffdcd4952b543d40c92bea6d
Amal <amalrajan30@gmail.com>
2022-04-09 12:52:34 +0530

    save scaped highlight with conf

--- src/getHighlight.ts
+++ src/getHighlight.ts
@@ -1,23 +1,25 @@
-import { Page } from "puppeteer";
-import fs from "fs-extra";
+import {Page} from 'puppeteer';
+import Conf from 'conf';
 
-interface Highlight {
+export interface Highlight {
   pageNo: string;
   highlight: string;
 }
 
-interface BookHighlight {
+export interface BookHighlight {
   bookTitle: string;
   highlights: Highlight[] | null;
 }
 
+const conf = new Conf();
+
 const getAllHighlights = async (page: Page) => {
   // get wrapper element
-  const container = await page.waitForSelector("#kp-notebook-annotations");
+  const container = await page.waitForSelector('#kp-notebook-annotations');
 
   // get all highlights in the container
   await page.waitForTimeout(10000);
-  const allHighlights = await container?.$$(".a-spacing-base");
+  const allHighlights = await container?.$$('.a-spacing-base');
   if (!allHighlights) {
     return null;
   }
@@ -27,18 +29,18 @@ const getAllHighlights = async (page: Page) => {
   console.log(`Saving ${allHighlights.length} highlights`);
   for (const highlightDiv of allHighlights) {
     const header = await (
-      await highlightDiv.$("#annotationHighlightHeader")
-    )?.evaluate((el) => el.textContent);
-    const pageNo = header?.split("|")[1]?.trim();
+      await highlightDiv.$('#annotationHighlightHeader')
+    )?.evaluate(el => el.textContent);
+    const pageNo = header?.split('|')[1]?.trim();
 
     // get content
     const highlight = await (
-      await highlightDiv.$("#highlight")
-    )?.evaluate((el) => el.textContent);
+      await highlightDiv.$('#highlight')
+    )?.evaluate(el => el.textContent);
 
     highlights = [
       ...highlights,
-      { pageNo: pageNo || "", highlight: highlight || "" },
+      {pageNo: pageNo || '', highlight: highlight || ''},
     ];
   }
 
@@ -46,24 +48,23 @@ const getAllHighlights = async (page: Page) => {
 };
 
 export const getHighLight = async (page: Page) => {
-  const allBooks = await page.$$("h2");
+  const allBooks = await page.$$('h2');
 
   let allHighlights: BookHighlight[] = [];
 
   for (const book of allBooks) {
-    await book.evaluate((el) => el.parentElement?.click());
-    const title = await book.evaluate((el) => el.textContent);
+    await book.evaluate(el => el.parentElement?.click());
+    const title = await book.evaluate(el => el.textContent);
     console.log(`Getting highlights for ${title}`);
 
     const highlights = await getAllHighlights(page);
     allHighlights = [
       ...allHighlights,
       {
-        bookTitle: title || "",
+        bookTitle: title || '',
         highlights,
       },
     ];
   }
-
-  fs.outputFileSync("./temp/highlights.json", JSON.stringify(allHighlights));
+  conf.set('highlights', allHighlights);
 };
